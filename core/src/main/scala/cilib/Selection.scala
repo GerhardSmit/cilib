64b87a3882866fdfa36abdc56530bcca8dcfb138
GerhardSmit <gsmit16@gmail.com>
2014-11-12 15:30:48 +0200

    Added gbest

--- core/src/main/scala/cilib/Selection.scala
+++ core/src/main/scala/cilib/Selection.scala
@@ -6,8 +6,19 @@ object Selection {
     (l: List[A], x: A) => {
       val size = l.size
       val y = n / 2
-      val point = (l.indexOf(x) - (n / 2) + size) % size
+      val point = (l.indexOf(x) - (y) + size) % size
 
       Stream.continually(l).flatten.drop(point).take(n).toList
     }
+
+  def gbest[A]: Selection[A] =
+  (l: List[A], x: A) => {
+  	if(l.contains(x))
+  		l
+  	else
+  		Nil  		
+  }
+
+  //def selfless
+    
 }
