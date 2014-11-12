9a7eaa094a5ee3a1a39e5dd2725959c85e25ef3e
GerhardSmit <gsmit16@gmail.com>
2014-11-12 16:26:10 +0200

    Added Selfless and started with von Neumann

--- core/src/main/scala/cilib/Selection.scala
+++ core/src/main/scala/cilib/Selection.scala
@@ -1,5 +1,7 @@
 package cilib
 
+import scala.math
+
 object Selection {
 
   def indexNeighbours[A](n: Int): Selection[A] =
@@ -12,13 +14,33 @@ object Selection {
     }
 
   def gbest[A]: Selection[A] =
-  (l: List[A], x: A) => {
-  	if(l.contains(x))
-  		l
-  	else
-  		Nil  		
+  (l: List[A], A) => {
+  		l		
   }
 
-  //def selfless
+  def selflessBest[A](n: Int): Selection[A] = 
+  (l: List[A], x: A) => {
+  	val lbest = indexNeighbours(n)(l,x)
+  	lbest.filter(_ != x)
+  }
+
+  def find[A](l: List[A],n: Int, r: Int, c: Int) : Int =
+  	l.indexOf(r * n + c) 
+
+  def f(r: Int,nRows: Int, np : Int, sqSide : Int) : Int =
+ 	if(r == nRows - 1)
+ 		np - r * sqSide
+ 	else
+ 		sqSide
+
+  def vonNeumannNeighbourhood[A] : Selection[A] =
+  (l: List[A], x: A) => {
+  	val np = l.size
+  	val index = l.indexOf(x)
+  	val sqSide = scala.math.round(scala.math.sqrt(np)).toInt
+  	val nRows = scala.math.ceil(np/sqSide.toDouble).toInt
+  	val row = (index / sqSide).toInt
+  	val col = (index % sqSide).toInt
+  }
     
 }
